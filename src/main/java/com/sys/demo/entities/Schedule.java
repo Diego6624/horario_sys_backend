ea1d53d160142004270d1eeeb60f18345527b688
Diego6624 <juan2525.2004@gmail.com>
2026-02-20 21:00:18 -0500

    Schedule

---
+++ src/main/java/com/sys/demo/entities/Schedule.java
@@ -0,0 +1,44 @@
+package com.sys.demo.entities;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "schedule")
+public class Schedule {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "numLab", nullable = false)
+    private String numLab;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private DayOfWeek dayOfWeek;
+
+    @Column(nullable = false)
+    private LocalTime startTime;
+
+    @Column(nullable = false)
+    private LocalTime endTime;
+
+    private String docente;
+
+    private String curso;
+
+    private String sesion;
+}
